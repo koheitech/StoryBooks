b6e9857452306e63af7bc1d056bd6da27a2e5bcb
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-16 14:21:49 +0900

    introduce 'morgan' logging when running in development mode

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 // loading config
@@ -10,6 +11,10 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 3000;
 
