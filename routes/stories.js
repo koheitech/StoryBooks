39572d2307483ba069b86205400a7cde5fb4458e
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-16 19:33:07 +0900

    create route for displaying and adding stories

---
+++ routes/stories.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+const Story = require('../models/Story');
+
+// @descr    Show add page
+// @route    GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add');
+});
+
+// @descr    Process add form
+// @route    POST /stories
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect('/dashboard');
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
+});
+
+
+module.exports = router;
