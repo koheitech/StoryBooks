6442bfd172a63d0ce2a1b77194beb093c670e8d5
Kohei Miyamoto <koheimiyamoto0801@gmail.com>
2021-11-16 14:57:33 +0900

    render the route into templating engine

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const router = express.Router();
 // @descr    Login/Landing page
 // @route    GET /
 router.get('/', (req, res) => {
-  res.send('Login');
+  res.render('login');
 });
 
 // @descr    Dashboard
 // @route    GET /dashboard
 router.get('/dashboard', (req, res) => {
-  res.send('Dashboard');
+  res.render('dashboard');
 });
 
 
